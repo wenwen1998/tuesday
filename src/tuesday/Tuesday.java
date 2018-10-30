c55c5fbedc9472bce89615a6b496a46195ed0d79
wenwen1998 <44257781+wenwen1998@users.noreply.github.com>
2018-10-30 14:56:05 +0800

    Update Tuesday.java

--- src/tuesday/Tuesday.java
+++ src/tuesday/Tuesday.java
@@ -16,6 +16,7 @@ public class Tuesday {
      */
     public static void main(String[] args) {
         System.out.println("Hello Friend!");
+        System.out.println("Good day!!");
     }
     
 }
